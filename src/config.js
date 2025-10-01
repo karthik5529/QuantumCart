7aa5d16766b06c563577baeac4e3cde1bd9ea1ac
karthik5529 <sambathram69@gmail.com>
2025-10-01 14:22:03 +0530

    Update React app for GitHub Pages deployment

--- src/config.js
+++ src/config.js
@@ -1,5 +1,16 @@
-export const API_BASE_URL = 'http://127.0.0.1:8000' ;
+// config.js
+export const API_BASE_URL = 'http://127.0.0.1:8000'; // backend link
+
+// Add the React app base path for GitHub Pages
+export const REACT_BASE_URL = '/QuantumCart';
 
 
+
+
+
+
+
+
+//export const API_BASE_URL = 'http://127.0.0.1:8000' ;
 //'http://127.0.0.1:8000' for pc
 //'http://10.176.55.253:8000' for mobile
